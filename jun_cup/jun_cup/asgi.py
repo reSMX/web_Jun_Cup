454f55266f001f8d471d9a161afe3c9f19cf98db
Timoface <timofey.ili@yandex.ru>
2025-04-08 21:55:08 +0700

    django project

---
+++ jun_cup/jun_cup/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jun_cup project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jun_cup.settings')
+
+application = get_asgi_application()
--- jun_cup/main/apps.py
+++ jun_cup/main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JunCupConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jun_cup'
+    name = 'main'
