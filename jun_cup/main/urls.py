092bf32392b3f593ffaa6a3b1c3b08cef9c81036
Timoface <timofey.ili@yandex.ru>
2025-04-08 23:26:08 +0700

    add registration page and sign detect page

--- jun_cup/main/urls.py
+++ jun_cup/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sign_detect/', views.sign_detect, name='sign_detect'),
+    path('registration/', views.registration, name='registration'),
 ]
--- jun_cup/main/views.py
+++ jun_cup/main/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+def sign_detect(request):
+    return render(request, 'main/sign_detect.html')
+
+
+def registration(request):
+    return render(request, 'main/registration.html')
